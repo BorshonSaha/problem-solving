573a6ff533534c0d6b366e97c71bfc1a20770970
Borshon Saha <saha11borshon@gmail.com>
2026-09-23 22:48:34 +0200

    TwoSum problem class name fixed

--- dsa-problems/src/array/TwoSum.java
+++ dsa-problems/src/array/TwoSum.java
@@ -1,7 +1,9 @@
 // Link: https://leetcode.com/problems/two-sum/description/
 // Brute Force Approach, Time: O(n^2), Space: O(1)
 
-class Solution {
+package array;
+
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++) {
